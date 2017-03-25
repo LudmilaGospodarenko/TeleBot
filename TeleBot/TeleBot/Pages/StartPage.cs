fd3aa077d7edb9ebe2a7521b1bdf467fad581114
LudmilaGospodarenko <cathadow@mail.ru>
2017-03-25 16:14:43 +0300

    001 - Main Page

---
+++ TeleBot/TeleBot/Pages/StartPage.cs
@@ -0,0 +1,90 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Support.UI;
+using System;
+using OpenQA.Selenium.Interactions;
+using System.Drawing;
+
+namespace TeleBot.Pages
+{
+    class StartPage
+    {
+        private IWebDriver driver;
+        const string url = "https://web.telegram.org/#/im";
+        const string channel = "@ChatWarsBot";
+         
+        [FindsBy(How = How.XPath, Using = "//*[@class='im_dialogs_search']/input")]
+        private IWebElement search;
+
+        [FindsBy(How = How.XPath, Using = "//*[@class='im_dialogs_search_clear tg_search_clear']")]
+        private IWebElement clearSearch;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='ng-app']/body/div[1]/div[2]/div/div[1]/div[2]/div/div[1]/div[2]")]
+        private IWebElement foundChannel;
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='ng-app']/body/div[1]/div[2]/div/div[1]/div[2]/div/div[1]/div[3]")]
+        private IWebElement nextChannel;
+
+        [FindsBy(How = How.ClassName, Using = "composer_rich_textarea")]
+        private IWebElement messageField;
+
+
+        [FindsBy(How = How.XPath, Using = "//*[text()=’Chat Wars’]")]
+        private IWebElement messages;
+
+        //*[@id="ng-app"]/body/div[1]/div[2]/div/div[2]/div[3]/div/div[2]/div[2]/div/div/form/div[2]/div[4]
+
+        public StartPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        public void GoToUrl()
+        {
+            driver.Navigate().GoToUrl(url);
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(2000));
+            IWebElement title = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@class='tg_head_btn dropdown-toggle']/i")));
+            var par = title.Location;
+            Console.WriteLine("title = {0}", par);
+        }
+
+        public void OpenChat()
+        {
+            search.SendKeys(channel);
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(2000));
+            IWebElement title = wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//*[@class='tg_head_btn dropdown-toggle']/i")));
+            new Actions(driver).MoveToElement(title).MoveByOffset(10, 140).Click().Perform();
+            Point clickedOn = title.Location;
+            clickedOn.Offset(10, 210);
+            Console.WriteLine("Clicked on = {0}", clickedOn);
+            new Actions(driver).MoveToElement(title).MoveByOffset(0, 0).Click().Perform();
+
+
+            new Actions(driver).MoveToElement(title).MoveByOffset(0, 0).Click().Perform();
+
+            //var selectedArea = foundChannel.Location;
+            //Console.WriteLine("Location of found item = {0}", selectedArea);
+            // foundChannel.Click();
+        }
+
+        public void SendMesage(string message)
+        {
+            messageField.SendKeys(message);
+        }
+
+        //public void FindChat()
+        //{
+        //    foreach (IWebElement chat in chatList)
+        //        if (chat.Text == channel)
+        //            chat.Click();
+        //        else Console.WriteLine("Element {0} wasn't found", channel);
+        //            //Thread.Sleep(10);
+        //channelFind.Click();
+        //search.SendKeys(channel);
+        //clearSearch.Click();
+        //search.SendKeys(channel);
+
+        //}
+    }
+}
---
+++ TeleBot/TeleBot/Tests/Initial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using TeleBot.Pages;
+using System.Threading;
+
+
+
+namespace TeleBot
+{
+    [TestFixture]
+    public class Initial
+    {
+        IWebDriver driver;
+
+        [TestCase]
+        public void Login()
+        {
+            ChromeOptions options = new ChromeOptions();
+            options.AddArgument("user-data-dir=C:/Users/Crazy/AppData/Local/Google/Chrome/User Data/Default");
+            driver = new ChromeDriver(options);
+
+            StartPage startPage = new StartPage(driver);
+            startPage.GoToUrl();
+            //Thread.Sleep(10);
+            startPage.OpenChat();
+            //startPage.SendMesage("/top");
+            //startPage.FindChat();
+
+           // driver.Quit();
+        }
+    }
+}
